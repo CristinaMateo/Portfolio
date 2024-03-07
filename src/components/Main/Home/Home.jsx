4e1bbba094035eb6115aa56816f8bc66c547f2a4
Cristina Mateo <cris.mateo.99@gmail.com>
2024-03-07 13:56:38 +0100

    links

--- src/components/Main/Home/Home.jsx
+++ src/components/Main/Home/Home.jsx
@@ -7,9 +7,20 @@ const Home = () => {
       <h2>¡Hola! Soy Cristina. </h2>
       <p>Junior Full Stack Developer apasionada por la tecnología. Con sólidos conocimientos en desarrollo web y habilidades Full Stack, estoy emocionada por contribuir al mundo tecnológico con soluciones creativas y explorar nuevas oportunidades de aprendizaje y colaboración.</p>
 
-      <a className="link" href="www.linkedin.com/in/mateo-cristina"> LinkedIn</a>
-      <a className="link" href="https://github.com/CristinaMateo">GitHub</a>
-      <a className="link" href="mailto:cris.mateo.99@gmail.com">cris.mateo.99@gmail.com</a>
+      <div className="links-container">
+        <a className="link smallGrid" href="https://linkedin.com/in/mateo-cristina">
+          <img src="src/assets/linkedin.png" alt="LinkedIn" />
+          LinkedIn
+        </a>
+        <a className="link smallGrid" href="https://github.com/CristinaMateo">
+        <img src="src/assets/github.png" alt="GitHub" />
+          GitHub
+        </a>
+        <a className="link smallGrid" href="mailto:cris.mateo.99@gmail.com">
+        <img src="src/assets/mail.png" alt="Mail" />
+          cris.mateo.99@gmail.com
+        </a>
+      </div>
     </section>
   );
 };
