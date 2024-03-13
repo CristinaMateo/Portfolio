ceececce79cf8c074f39a176b078524e2eb8256f
Cristina Mateo <cris.mateo.99@gmail.com>
2024-03-13 13:56:28 +0100

    footer ans styles

--- src/components/Main/About/About.jsx
+++ src/components/Main/About/About.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const About = () => {
   return (
-    <section>
+    <section className="about">
       <h1>Acerca de mí </h1>
       <p>Hola, soy Cristina, una Junior Full Stack Developer. Mi fascinación por la tecnología me llevó a un bootcamp de desarrollo web, donde me sumergí en la creación de soluciones digitales innovadoras.
       </p>
