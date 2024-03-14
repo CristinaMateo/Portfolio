c2cb6689a9e5d6c65822af65065c963555af23f5
Cristina Mateo <cris.mateo.99@gmail.com>
2024-03-14 14:19:40 +0100

    experiencia y educación

--- src/components/Main/Experiencia/Experiencia.jsx
+++ src/components/Main/Experiencia/Experiencia.jsx
@@ -4,8 +4,31 @@ const Experiencia = () => {
   return (
     <section>
       <h1>Experiencia</h1>
-      <p>Próximamente...
-      </p>
+      <h3>Experiencia Laboral</h3>
+      <h5>INFORMADORA | CC EL TRIANGLE | JULIO 2021 - AGOSTO 2023 </h5>
+      <ul>
+        <li>Atención al cliente</li>
+        <li>Tareas administrativas</li>
+        <li>Informes para la gerencia del centro</li>
+      </ul>
+
+
+      <h3>Educación</h3>
+      <h5>BOOTCAMP FULLSTACK DEVELOPMENT <br />
+        The Bridge | Septiembre 2023 – Enero 2024</h5>
+      <ul>
+        <li>Layout: HTML5, CSS3, SASS, responsive and mobile-first design.</li>
+        <li>JavaScript (ES6) y servicios de terceros (APIS’s) </li>
+        <li>Base de datos con MongoDB y MySQL. </li>
+        <li>Creación de SPA’s con React. </li>
+        <li>Desarrollo Backend con NodeJS y ExpressJS</li>
+      </ul>
+      <h5>MÁSTER DE FORMACIÓN DE PROFESORADO (ESPECIALIDAD DE CATALÁN Y CASTELLANO) <br />
+        Universidad Autónoma de Barcelona | Octubre 2021 – Junio 2022</h5>
+
+      <h5>GRADO EN FILOLOGÍA HISPÁNICA (MENCIÓN EN LENGUA) <br />
+        Universidad de Barcelona | Septiembre 2017 – Junio 2021 </h5>
+
     </section>
   );
 };
