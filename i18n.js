370aa0f3d851beb97dfeb89546611b87ef0d6702
Cristina Mateo <cris.mateo.99@gmail.com>
2024-03-15 18:51:11 +0100

    centrar botones, detectar idioma

--- i18n.js
+++ i18n.js
@@ -5,22 +5,30 @@ import { initReactI18next } from 'react-i18next';
 import translationES from './locales/es/translation.json';
 import translationEN from './locales/en/translation.json';
 
+// Función para detectar el idioma del navegador
+function detectarIdiomaNavegador() {
+  const idiomaNavegador = navigator.language || navigator.languages[0];
+  return idiomaNavegador;
+}
+
+// Configuración de traducción
 i18n
-  .use(initReactI18next)
-  .init({
-    resources: {
-      en: {
-        translation: translationEN,
-      },
-      es: {
-        translation: translationES,
-      },
+.use(initReactI18next)
+.init({
+  resources: {
+    en: {
+      translation: translationEN,
     },
-    lng: 'es', // Idioma predeterminado
-    fallbackLng: 'en', // Idioma de fallback
-    interpolation: {
-      escapeValue: false,
-    },
-  });
+    es: {
+      translation: translationES,
+    }
+  },
+  lng: detectarIdiomaNavegador(), // Establecer el idioma predeterminado como el idioma del navegador
+  fallbackLng: 'en', // Idioma de fallback
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
 
 export default i18n;
